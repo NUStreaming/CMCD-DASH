4dd443edfba8c294853ea34196fed60dab4cfe9c
May Lim <mllim1719@gmail.com>
2020-11-30 16:48:18 +0800

    Updated profiles, njs config and result script

---
+++ dash-test-batch/results/generate_summary.py
@@ -0,0 +1,66 @@
+import os
+import json
+
+metrics = ["averageBitrate", "numSwitches", "stallDurationMs", "numStalls", "averageBufferLength"]
+summaryResults = {}
+
+folder = input("Enter test folder ('XXXXX_multiple' folders only): ")
+
+summaryFile = os.path.join(folder, "summary.txt")
+if os.path.exists(summaryFile):
+    os.remove(summaryFile)	# to replace previously generated file
+                     
+def printToConsoleAndFile(msg):
+    print(msg)
+    with open(summaryFile, "a+") as f:
+        f.write(msg + "\n")
+
+for root, dirs, files in os.walk(folder):
+    for name in files:
+        if "metrics-overall.json" in name:
+            _file = os.path.join(root, name)
+            with open(_file) as json_file:
+                 data = json.load(json_file)
+                 printToConsoleAndFile("")
+                 printToConsoleAndFile("------------------------------------------------------")
+                 printToConsoleAndFile(_file)
+                 printToConsoleAndFile("-------------------------------------------------------")
+                 for metric in  metrics:
+                     printToConsoleAndFile(metric + ": " + str(data[metric]))
+                     if metric not in summaryResults:
+                         summaryResults[metric] = {
+                             "sum": 0,
+                             "count": 0,
+                             "min": None,
+                             "max": None
+                         }
+                     summaryResults[metric]["sum"] += data[metric]
+                     summaryResults[metric]["count"] += 1
+                     if summaryResults[metric]["min"] == None or data[metric] < summaryResults[metric]["min"]:
+                         summaryResults[metric]["min"] = data[metric]
+                     if summaryResults[metric]["max"] == None or data[metric] > summaryResults[metric]["max"]:
+                         summaryResults[metric]["max"] = data[metric]
+
+printToConsoleAndFile("")
+printToConsoleAndFile("------------------------------------------------------")
+printToConsoleAndFile("SUMMARY")
+printToConsoleAndFile("-------------------------------------------------------")
+for metric, obj in summaryResults.items():
+     printToConsoleAndFile(metric + ": ")
+     printToConsoleAndFile("- average: " + str(summaryResults[metric]["sum"] / summaryResults[metric]["count"]))
+     for key, value in obj.items():
+        printToConsoleAndFile("- " + key + ": " + str(value))
+
+ 
+comments = None
+
+for root, dirs, files in os.walk(folder):
+    for name in files:
+        if "evaluate.json" in name and comments == None:
+            _file = os.path.join(root, name)
+            with open(_file) as json_file:
+                data = json.load(json_file)
+                comments = data["comments"]
+                printToConsoleAndFile("")
+                printToConsoleAndFile("# Comments for run: " + comments)
+                break
