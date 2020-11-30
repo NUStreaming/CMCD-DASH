2bcd1bc6eeaa2c657474c35e1046c64a86053400
May Lim <mllim1719@gmail.com>
2020-11-30 13:02:22 +0800

    Updated profile

--- dash-test-batch/tc-network-profiles/Cascade.sh
+++ dash-test-batch/tc-network-profiles/Cascade.sh
@@ -3,61 +3,61 @@
 for (( c=1; c>0; c++ ))
 do
 
-#sudo tc qdisc del dev enp5s0f0 root
-sudo tc qdisc add dev enp5s0f0 root handle 1: htb default 10
-sudo tc class add dev enp5s0f0 parent 1: classid 1:1 htb rate 50000kbit
-sudo tc class add dev enp5s0f0 parent 1:1 classid 1:10 htb rate 50000kbit
-sudo tc qdisc add dev enp5s0f0 parent 1:10 handle 10: sfq perturb 10
-sudo tc filter add dev enp5s0f0 parent 1:0 protocol ip prio 1 u32 \
+#sudo tc qdisc del dev enp0s3 root
+sudo tc qdisc add dev enp0s3 root handle 1: htb default 10
+sudo tc class add dev enp0s3 parent 1: classid 1:1 htb rate 50000kbit
+sudo tc class add dev enp0s3 parent 1:1 classid 1:10 htb rate 50000kbit
+sudo tc qdisc add dev enp0s3 parent 1:10 handle 10: sfq perturb 10
+sudo tc filter add dev enp0s3 parent 1:0 protocol ip prio 1 u32 \
 match ip dport 80 0xffff flowid 1:10
 sleep 30s
 
-sudo tc qdisc del dev enp5s0f0 root
-sudo tc qdisc add dev enp5s0f0 root handle 1: htb default 10
-sudo tc class add dev enp5s0f0 parent 1: classid 1:1 htb rate 20000kbit
-sudo tc class add dev enp5s0f0 parent 1:1 classid 1:10 htb rate 20000kbit
-sudo tc qdisc add dev enp5s0f0 parent 1:10 handle 10: sfq perturb 10
-sudo tc filter add dev enp5s0f0 parent 1:0 protocol ip prio 1 u32 \
+sudo tc qdisc del dev enp0s3 root
+sudo tc qdisc add dev enp0s3 root handle 1: htb default 10
+sudo tc class add dev enp0s3 parent 1: classid 1:1 htb rate 20000kbit
+sudo tc class add dev enp0s3 parent 1:1 classid 1:10 htb rate 20000kbit
+sudo tc qdisc add dev enp0s3 parent 1:10 handle 10: sfq perturb 10
+sudo tc filter add dev enp0s3 parent 1:0 protocol ip prio 1 u32 \
 match ip dport 80 0xffff flowid 1:10
 sleep 30s
 
-sudo tc qdisc del dev enp5s0f0 root
-sudo tc qdisc add dev enp5s0f0 root handle 1: htb default 10
-sudo tc class add dev enp5s0f0 parent 1: classid 1:1 htb rate 10000kbit
-sudo tc class add dev enp5s0f0 parent 1:1 classid 1:10 htb rate 10000kbit
-sudo tc qdisc add dev enp5s0f0 parent 1:10 handle 10: sfq perturb 10
-sudo tc filter add dev enp5s0f0 parent 1:0 protocol ip prio 1 u32 \
+sudo tc qdisc del dev enp0s3 root
+sudo tc qdisc add dev enp0s3 root handle 1: htb default 10
+sudo tc class add dev enp0s3 parent 1: classid 1:1 htb rate 10000kbit
+sudo tc class add dev enp0s3 parent 1:1 classid 1:10 htb rate 10000kbit
+sudo tc qdisc add dev enp0s3 parent 1:10 handle 10: sfq perturb 10
+sudo tc filter add dev enp0s3 parent 1:0 protocol ip prio 1 u32 \
 match ip dport 80 0xffff flowid 1:10
 sleep 30s
 
-sudo tc qdisc del dev enp5s0f0 root
-sudo tc qdisc add dev enp5s0f0 root handle 1: htb default 10
-sudo tc class add dev enp5s0f0 parent 1: classid 1:1 htb rate 5000kbit
-sudo tc class add dev enp5s0f0 parent 1:1 classid 1:10 htb rate 5000kbit
-sudo tc qdisc add dev enp5s0f0 parent 1:10 handle 10: sfq perturb 10
-sudo tc filter add dev enp5s0f0 parent 1:0 protocol ip prio 1 u32 \
+sudo tc qdisc del dev enp0s3 root
+sudo tc qdisc add dev enp0s3 root handle 1: htb default 10
+sudo tc class add dev enp0s3 parent 1: classid 1:1 htb rate 5000kbit
+sudo tc class add dev enp0s3 parent 1:1 classid 1:10 htb rate 5000kbit
+sudo tc qdisc add dev enp0s3 parent 1:10 handle 10: sfq perturb 10
+sudo tc filter add dev enp0s3 parent 1:0 protocol ip prio 1 u32 \
 match ip dport 80 0xffff flowid 1:10
 sleep 30s
 
-sudo tc qdisc del dev enp5s0f0 root
-sudo tc qdisc add dev enp5s0f0 root handle 1: htb default 10
-sudo tc class add dev enp5s0f0 parent 1: classid 1:1 htb rate 10000kbit
-sudo tc class add dev enp5s0f0 parent 1:1 classid 1:10 htb rate 10000kbit
-sudo tc qdisc add dev enp5s0f0 parent 1:10 handle 10: sfq perturb 10
-sudo tc filter add dev enp5s0f0 parent 1:0 protocol ip prio 1 u32 \
+sudo tc qdisc del dev enp0s3 root
+sudo tc qdisc add dev enp0s3 root handle 1: htb default 10
+sudo tc class add dev enp0s3 parent 1: classid 1:1 htb rate 10000kbit
+sudo tc class add dev enp0s3 parent 1:1 classid 1:10 htb rate 10000kbit
+sudo tc qdisc add dev enp0s3 parent 1:10 handle 10: sfq perturb 10
+sudo tc filter add dev enp0s3 parent 1:0 protocol ip prio 1 u32 \
 match ip dport 80 0xffff flowid 1:10
 sleep 30s
 
-sudo tc qdisc del dev enp5s0f0 root
-sudo tc qdisc add dev enp5s0f0 root handle 1: htb default 10
-sudo tc class add dev enp5s0f0 parent 1: classid 1:1 htb rate 20000kbit
-sudo tc class add dev enp5s0f0 parent 1:1 classid 1:10 htb rate 20000kbit
-sudo tc qdisc add dev enp5s0f0 parent 1:10 handle 10: sfq perturb 10
-sudo tc filter add dev enp5s0f0 parent 1:0 protocol ip prio 1 u32 \
+sudo tc qdisc del dev enp0s3 root
+sudo tc qdisc add dev enp0s3 root handle 1: htb default 10
+sudo tc class add dev enp0s3 parent 1: classid 1:1 htb rate 20000kbit
+sudo tc class add dev enp0s3 parent 1:1 classid 1:10 htb rate 20000kbit
+sudo tc qdisc add dev enp0s3 parent 1:10 handle 10: sfq perturb 10
+sudo tc filter add dev enp0s3 parent 1:0 protocol ip prio 1 u32 \
 match ip dport 80 0xffff flowid 1:10
 sleep 30s
 
 
 
-sudo tc qdisc del dev enp5s0f0 root
+sudo tc qdisc del dev enp0s3 root
 done
